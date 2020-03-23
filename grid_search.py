de72c14d98caf48adb6b1ced34974f8cb6f97a19
Marine Huynh <marine.huynh@yahoo.ca>
2020-03-22 23:03:39 -0400

    added grid search on both cpu and gpu

--- grid_search.py
+++ grid_search.py
@@ -1,50 +1,48 @@
 import itertools as it
 import subprocess
+import sys
 
 def getGridSearch(optim):
+    set = ['inter', 'intra']
     patience = [10, 25, 50]
     kfold = [5, 10, 15]
     epochs = [100, 150, 200, 250, 300]
     optim = [optim]
     lr = [0.001, 0.0001, 0.00001, 0.000001]
     batch_size = [1, 2, 4, 8, 16]
+    randomJointOrder = [1, 5, 10]
 
+    # always true for now
     changeOrder = ['true', 'false']
-    randomJointOrder = []
 
-    my_dict = {'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size}
-
-    allNames = sorted(my_dict)
-    combinationsAdams = it.product(*(my_dict[Name] for Name in allNames))
-
-    # print(list(combinationsAdams))
-    # print(len(list(combinationsAdams)))
-
-    subprocess.check_output(["echo", "Hello World!"])
-
+    my_dict = {'set': set, 'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size, 'randomJointOrder': randomJointOrder}
+    combinationsAdams = it.product(*(my_dict[Name] for Name in my_dict))
     return list(combinationsAdams)
 
 if __name__ == '__main__':
+    argumentsPassed = sys.argv
+    default_cmd = "python SuperRNModel.py "
     hyperparamsCombinationList = getGridSearch('Adam')
     count = 0
+
+    # loop over all possible combinations of hyperparameters
     for element in hyperparamsCombinationList:
 
-        combHyperparams = '--batch_size ' + str(element[0]) + ' --epoch ' + str(element[1]) + ' --kfold ' + str(element[2]) + ' --lr ' + str(element[3]) + ' --optim ' + str(element[4]) + ' --patience ' + str(element[5])
+        combHyperparams = '--patience ' + str(element[1]) + ' --kfold ' + str(element[2]) + ' --epochs ' + str(element[3]) + ' --optim ' + str(element[4]) + ' --lr ' + str(element[5]) + ' --batch_size ' + str(element[6]) + ' --randomJointOrder ' + str(element[7])
         #subprocess.check_call(["python SuperRNModel.py --intra --changeOrder --randomJointOrder 1 --epochs 20 --kfold 10 --dataset SBU --batch_size 32"])
 
-        default_cmd = "python SuperRNModel.py --intra --changeOrder --randomJointOrder 1 --dataset PHYT "
+        if '--full_gpu' in argumentsPassed:
+            default_cmd = default_cmd + "--full_gpu "
+        if element[0] == 'intra':
+            default_cmd = default_cmd + "--intra --changeOrder --dataset PHYT "
+        elif element[0] == 'inter':
+            default_cmd = default_cmd + "--inter --changeOrder --dataset PHYT "
         command = default_cmd + combHyperparams
-        subprocess.call(command)
+        subprocess.call(command, shell=True)
+
         count = count + 1
         if count == 2:
             break
-    # python
-    # SuperRNModel.py - -intra - -changeOrder - -randomJointOrder
-    # 1 - -epochs
-    # 20 - -kfold
-    # 10 - -dataset
-    # SBU - -full_gpu - -batch_size
-    # 32
 
 # second combinations for SGD
 # optim = ['SGD']
@@ -52,10 +50,7 @@ if __name__ == '__main__':
 # nesterov = []
 #
 # my_dict = {'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size, 'momemtum': momemtum, 'nesterov': nesterov}
-# allNames = sorted(my_dict)
-# combinationsSGD = it.product(*(my_dict[Name] for Name in allNames))
-# print(len(list(combinationsSGD)))
-# print(list(combinationsSGD))
+# combinationsSGD = it.product(*(my_dict[Name] for Name in my_dict))
 #
+# TODO: add the two together to run over all (Adam, SGD)
 # combinations = combinationsAdams.append(combinationsSGD)
-# print(len(list(combinations)))

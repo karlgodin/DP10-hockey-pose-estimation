5bb173aa0bf0e318b21de0921448e96b324b449f
Marine Huynh <marine.huynh@yahoo.ca>
2020-03-24 00:10:33 -0400

    added grid search for SGD + range of comb

--- grid_search.py
+++ grid_search.py
@@ -2,34 +2,51 @@ import itertools as it
 import subprocess
 import sys
 
-def getGridSearch(optim):
+def getGridSearch(optimizer):
+    my_dict = {}
     set = ['inter', 'intra']
     patience = [10, 25, 50]
     kfold = [5, 10, 15]
-    epochs = [100, 150, 200, 250, 300]
-    optim = [optim]
-    lr = [0.001, 0.0001, 0.00001, 0.000001]
+    epochs = [1]
+    optim = [optimizer]
+    lr = [0.0001, 0.00001, 0.000001]
     batch_size = [1, 2, 4, 8, 16]
     randomJointOrder = [1, 5, 10]
+    momentum = [0.1]
+    nesterov = [True, False]
 
     # always true for now
     changeOrder = ['true', 'false']
 
-    my_dict = {'set': set, 'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size, 'randomJointOrder': randomJointOrder}
-    combinationsAdams = it.product(*(my_dict[Name] for Name in my_dict))
-    return list(combinationsAdams)
+    if optimizer == 'Adam':
+        my_dict = {'set': set, 'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size, 'randomJointOrder': randomJointOrder}
+    elif optimizer == "SGD":
+        my_dict = {'set': set, 'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr,
+                   'batch_size': batch_size, 'randomJointOrder': randomJointOrder, 'momentum': momentum, 'nesterov': nesterov}
+    combinations = it.product(*(my_dict[Name] for Name in my_dict))
+    return list(combinations)
 
 if __name__ == '__main__':
+    combHyperparams = ''
     argumentsPassed = sys.argv
     default_cmd = "python SuperRNModel.py "
-    hyperparamsCombinationList = getGridSearch('Adam')
-    count = 0
+    # knowing the order of the command, the forth argument (not counting python) will always be the optim
+    hyperparamsCombinationList = getGridSearch(str(argumentsPassed[2]))
+
+    # break the list of combinations to run fewer at a time
+    hyperparamsCombinationList = hyperparamsCombinationList[int(argumentsPassed[3]): int(argumentsPassed[4])]
 
     # loop over all possible combinations of hyperparameters
     for element in hyperparamsCombinationList:
+        if 'Adam' in argumentsPassed:
+            combHyperparams = '--patience ' + str(element[1]) + ' --kfold ' + str(element[2]) + ' --epochs ' + str(element[3]) + ' --optim ' + str(element[4]) + ' --lr ' + str(element[5]) + ' --batch_size ' + str(element[6]) + ' --randomJointOrder ' + str(element[7])
+        elif 'SGD' in argumentsPassed:
+            combHyperparams = '--patience ' + str(element[1]) + ' --kfold ' + str(element[2]) + ' --epochs ' + str(
+                element[3]) + ' --optim ' + str(element[4]) + ' --lr ' + str(element[5]) + ' --batch_size ' + str(
+                element[6]) + ' --randomJointOrder ' + str(element[7]) + ' --momentum ' + str(element[8])
 
-        combHyperparams = '--patience ' + str(element[1]) + ' --kfold ' + str(element[2]) + ' --epochs ' + str(element[3]) + ' --optim ' + str(element[4]) + ' --lr ' + str(element[5]) + ' --batch_size ' + str(element[6]) + ' --randomJointOrder ' + str(element[7])
-        #subprocess.check_call(["python SuperRNModel.py --intra --changeOrder --randomJointOrder 1 --epochs 20 --kfold 10 --dataset SBU --batch_size 32"])
+            if element[9] == True:
+                combHyperparams = combHyperparams + ' --nesterov'
 
         if '--full_gpu' in argumentsPassed:
             default_cmd = default_cmd + "--full_gpu "
@@ -39,18 +56,3 @@ if __name__ == '__main__':
             default_cmd = default_cmd + "--inter --changeOrder --dataset PHYT "
         command = default_cmd + combHyperparams
         subprocess.call(command, shell=True)
-
-        count = count + 1
-        if count == 2:
-            break
-
-# second combinations for SGD
-# optim = ['SGD']
-# momemtum = []
-# nesterov = []
-#
-# my_dict = {'patience': patience, 'kfold': kfold, 'epochs': epochs, 'optim': optim, 'lr': lr, 'batch_size': batch_size, 'momemtum': momemtum, 'nesterov': nesterov}
-# combinationsSGD = it.product(*(my_dict[Name] for Name in my_dict))
-#
-# TODO: add the two together to run over all (Adam, SGD)
-# combinations = combinationsAdams.append(combinationsSGD)

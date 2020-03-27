fcf3ea2d0f63a7f2328657c83d10751b48976f20
ShawnVosburg <shawn.vosburg@mail.mcgill.ca>
2020-03-27 12:51:16 -0400

    wow, i didnt fix the bug before

--- runTODO.py
+++ runTODO.py
@@ -19,6 +19,7 @@ if(not os.path.exists('../drive/My Drive/DesignProject/')):
 if(os.path.exists(DONEPath)):
     with open(DONEPath) as f:
         done = int(f.read())
+        done += 1
 else:
     done = 0
 
